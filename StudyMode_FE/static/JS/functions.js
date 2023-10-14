624eb8f075761611c8dec2118ce6d92b8a4213f0
Rene-Capistran <rene.capistran02@utrgv.edu>
2023-10-14 04:26:47 -0500

    General Commit

--- StudyMode_FE/static/JS/functions.js
+++ StudyMode_FE/static/JS/functions.js
@@ -5,17 +5,32 @@ function sayHello() {
  }
 
  function toggleDimness() {
-    var bg = document.body.style.background;
-    var bgColor = getComputedStyle(document.body).backgroundColor
     if(screenDimmed) screenDimmed = false;
     else screenDimmed = true;
 
     if(screenDimmed)
     {
-        document.body.style.background = "#e2ebec";
+        document.body.style.background = "#596566";
+
+        elements = document.getElementsByClassName("Container")
+        for(var i = 0; i < elements.length; i++)
+        {
+            elements[i].style.backgroundColor = "#a8c2b8"
+        }
+
+        document.getElementById("TimerButton").textContent = "Resume"
+
     }
     else
     {
-        document.body.style.background = "#596566";
+        document.body.style.background = "#e2ebec";
+
+        elements = document.getElementsByClassName("Container")
+        for(var i = 0; i < elements.length; i++)
+        {
+            elements[i].style.backgroundColor = "#C3D5CE"
+        }
+
+        document.getElementById("TimerButton").textContent = "Pause"
     }
  }
