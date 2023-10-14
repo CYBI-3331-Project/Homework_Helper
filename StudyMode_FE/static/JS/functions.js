17e400b092738b019cfc9d13ae685b7f5cb9fa26
Rene-Capistran <rene.capistran02@utrgv.edu>
2023-10-14 04:48:37 -0500

    Added comments

--- StudyMode_FE/static/JS/functions.js
+++ StudyMode_FE/static/JS/functions.js
@@ -1,36 +1,55 @@
+//Script for JS functions to be used in the webapp
+
+//Vars
 var screenDimmed = false
 
+
+//Test Function
 function sayHello() {
     alert("Hello World")
  }
 
+ //Toggle Study Mode screen dimness
  function toggleDimness() {
     if(screenDimmed) screenDimmed = false;
     else screenDimmed = true;
 
-    if(screenDimmed)
+    
+    if(screenDimmed) //If screen is to be dimmed
     {
+        //Change the background color  of the body to a darker version of itself
         document.body.style.background = "#596566";
 
+        //Find every element with the 'Container' class, and put them into an array called 'elements'
         elements = document.getElementsByClassName("Container")
+
+        //For loop to iterate through each element within the array
         for(var i = 0; i < elements.length; i++)
         {
+            //For each element within the array, change it's color to a darker version
             elements[i].style.backgroundColor = "#a8c2b8"
         }
 
+        //Change the text of the button from 'Pause' to 'Resume
         document.getElementById("TimerButton").textContent = "Resume"
 
     }
-    else
+    else //Otherwise, if screen is to be undimmed
     {
+        //Change the background color  of the body to a lighter version of itself
         document.body.style.background = "#e2ebec";
 
+        //Find every element with the 'Container' class, and put them into an array called 'elements'
         elements = document.getElementsByClassName("Container")
+
+        //For loop to iterate through each element within the array
         for(var i = 0; i < elements.length; i++)
         {
+            //For each element within the array, change it's color to a lighter version
             elements[i].style.backgroundColor = "#C3D5CE"
         }
 
+        //Change the text of the button from 'Pause' to 'Resume
         document.getElementById("TimerButton").textContent = "Pause"
     }
  }
