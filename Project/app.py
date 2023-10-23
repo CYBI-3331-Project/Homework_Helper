62fd94b137200bbed40f48dd2b7fee18ee8353e3
Dantheman85427 <125824408+Dantheman85427@users.noreply.github.com>
2023-10-23 14:37:49 -0500

    Cleaned up and organized files

---
+++ Project/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, Response
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def log_in():
+    return render_template('log_in.html')
+
+@app.route('/Create_your_account/')
+def create():
+    return render_template('create.html')
+
+@app.route('/Forgot_Password/')
+def forgotpw():
+    return render_template('forgotpw.html')
+
+@app.route('/Homepage/')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/Homepage/Study_Mode/')
+def study_mode():
+    return render_template('study_mode.html')
+
+if __name__ == "__main__":
+    app.run(host='192.168.1.142')
